393ad7de9221757685dcb1ea09e2801fd5d534b1
galvao <fgalvao90@yahoo.com>
2021-09-11 00:26:05 +0100

    return json errors

--- app/Http/Requests/TaskRequest.php
+++ app/Http/Requests/TaskRequest.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
 
 class TaskRequest extends FormRequest
 {
@@ -26,5 +30,21 @@ class TaskRequest extends FormRequest
         return [
             'summary' => 'bail|required|string|min:3|max:2500'
         ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     *
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY)
+        );
     }
 }
