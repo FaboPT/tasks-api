9044aaff6035d1e589aba15659e0533dff19ba34
Fábio Galvão <fabiogalvao@MacBook-Pro.Home>
2021-11-19 21:35:47 +0000

    Clean code In Authenticate.php Middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Middleware;
 
+use App\Traits\ResponseAPI;
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class Authenticate extends Middleware
 {
+    use ResponseAPI;
     public function handle($request, Closure $next, ...$guards)
     {
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
             if ($this->auth->guard($guard)->guest()) {
-                return response()->json(["message" => "Unauthorized."], 401);
+                return $this->error("Unauthorized.", Response::HTTP_UNAUTHORIZED);
             }
         }
 
